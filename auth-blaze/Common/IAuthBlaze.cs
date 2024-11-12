355cb0aedcd0da2cece9381dbbe6bb15750ab9a8
Frank Mensah <33913399+menfra@users.noreply.github.com>
2024-11-12 22:55:04 +0100

    [AUTH-233] .Net 9 upgrade

---
+++ auth-blaze/Common/IAuthBlaze.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auth_blaze.Common
+{
+    public interface IAuthBlaze
+    {
+        Task<bool> AuthenticateAsync(string username, string password);
+        Task<bool> EnforceMFAAsync(string username);
+        bool IsGeolocationAllowed(HttpContext context);
+        bool ValidateToken(string token);
+    }
+}
---
+++ auth-blaze/Configuration/AuthBlazeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using auth_blaze.Common;
+using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace auth_blaze.Configuration
+{
+    public static class AuthBlazeExtensions
+    {
+        public static IApplicationBuilder UseAuthBlaze(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthBlazeMiddleware>();
+        }
+    }
+}
